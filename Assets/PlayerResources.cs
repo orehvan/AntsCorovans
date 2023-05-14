c79b3aa3e41d381920064b52739fbae5a271561a
lessy2014 <lessik03@gmail.com>
2023-05-15 02:34:03 +0500

    Resources WIP

---
+++ Assets/PlayerResources.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResources : MonoBehaviour
+{
+    private int fireResource;
+    private int poisonResource;
+    private int metalResource;
+
+    public int FireResource
+    {
+        get => fireResource;
+        set
+        {
+            fireResource = value;
+            FireChanged?.Invoke(fireResource);
+        }
+    }
+
+    public int PoisonResource
+    {
+        get => poisonResource;
+        set
+        {
+            poisonResource = value;
+            PoisonChanged?.Invoke(poisonResource);
+        }
+    }
+
+    public int MetalResource
+    {
+        get => metalResource;
+        set
+        {
+            metalResource = value;
+            MetalChanged?.Invoke(metalResource);
+        }
+    }
+    
+    public event Action<int> PoisonChanged;
+
+    public event Action<int> FireChanged;
+
+    public event Action<int> MetalChanged;
+}
