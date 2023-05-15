a35d8df1e52278efb83742ca1bdef0f8e72fc9da
i.orexin <nutsmany@mail.ru>
2023-05-16 01:41:59 +0500

    terrain destruction (kinda)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DTerrain;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    [FormerlySerializedAs("DestroyCircleSize")] public int destroyCircleSize;
 
+    private Shape DestroyCircle;
     private Vector3 TargetPosition;
     private bool isMoving;
     
     [SerializeField] protected BasicPaintableLayer primaryLayer;
-    void Update()
+    [SerializeField] protected BasicPaintableLayer secondaryLayer;
+
+    private void Start()
+    {
+        // DestroyCircleSize = (int)Math.Ceiling(GetComponent<SpriteRenderer>().bounds.size.x);
+        // destroyCircleSize = 40;
+        DestroyCircle = Shape.GenerateShapeCircle(destroyCircleSize);
+        
+        Debug.Log($"Circle size: {destroyCircleSize}");
+    }
+
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             SetTargetPosition();
         }
 
-        if (isMoving)
-        {
-            Move();
-        }
+        if (!isMoving) return;
+        Move();
+        DestroyTerrain();
     }
 
     private void Move()
@@ -40,5 +54,27 @@ public class PlayerController : MonoBehaviour
         TargetPosition.z = transform.position.z;
 
         isMoving = true;
+    }
+
+    private void DestroyTerrain()
+    {
+        primaryLayer?.Paint(new PaintingParameters()
+        {
+            Color = Color.clear,
+            Position = new Vector2Int((int)(transform.position.x * primaryLayer.PPU) - destroyCircleSize,
+                (int)(transform.position.y * primaryLayer.PPU) - destroyCircleSize),
+            Shape = DestroyCircle,
+            PaintingMode = PaintingMode.REPLACE_COLOR,
+            DestructionMode = DestructionMode.DESTROY
+        });
+        secondaryLayer?.Paint(new PaintingParameters()
+        {
+            Color = Color.clear,
+            Position = new Vector2Int((int)(transform.position.x * secondaryLayer.PPU) - destroyCircleSize,
+                (int)(transform.position.y * secondaryLayer.PPU) - destroyCircleSize),
+            Shape = DestroyCircle,
+            PaintingMode = PaintingMode.REPLACE_COLOR,
+            DestructionMode = DestructionMode.DESTROY
+        });
     }
 }
