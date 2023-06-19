0fc4b4473a11d780222e8322364fc6531f1f54ad
i.orexin <nutsmany@mail.ru>
2023-06-20 02:13:26 +0500

    new map by the best level designer

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,41 +1,13 @@
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseDelta;
+    [SerializeField] private Transform playerTransform;
 
-    public float moveSpeed;
-    public float zoomSpeed;
-
-    public float minZoomDistance;
-    public float maxZoomDistance;
-
-    // Update is called once per frame
     private void Update()
     {
-        Move();
-        Zoom();
-    }
-
-    private void Move()
-    {
-        var mouseInput = Input.mousePosition;
-
-        if (mouseInput.x >= Screen.width - mouseDelta)
-            ApplyVector(Vector3.right);
-        else if (mouseInput.x <= mouseDelta) ApplyVector(Vector3.left);
-
-        if (mouseInput.y >= Screen.height - mouseDelta) ApplyVector(Vector3.up);
-        else if (mouseInput.y <= mouseDelta) ApplyVector(Vector3.down);
-        /*
-        transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * moveSpeed * Time.deltaTime,
-            Input.GetAxisRaw("Mouse Y") * moveSpeed * Time.deltaTime, 0f);
-    */
-
-        void ApplyVector(Vector3 vector) => transform.position += vector * (moveSpeed * Time.deltaTime);
-    }
-
-    private void Zoom()
-    {
+        var position = playerTransform.position;
+        transform.position = new Vector3(position.x, position.y, transform.position.z);
     }
 }
--- Assets/Scripts/CorovanSpawn.cs
+++ Assets/Scripts/CorovanSpawn.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 public class CorovanSpawn : MonoBehaviour
 {
     [SerializeField] private CorovanController corovan;
+    [SerializeField] private GameObject corovanPrefab;
+    [SerializeField] private Transform spawnPoint;
     private bool spawned;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (spawned) return;
         spawned = true;
-        corovan.gameObject.SetActive(true);
+        var position = spawnPoint.position;
+        Instantiate(corovanPrefab,
+            new Vector3(position.x, position.y, position.z),
+            new Quaternion(1, 1, 1, 1));
+        
+        //corovan.gameObject.SetActive(true);
     }
 }
